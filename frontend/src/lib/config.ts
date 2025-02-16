c7d371924d1ad24011ed5b281d41c9ea1ce03acc
khalidkhnz <eternalkhalidkhnz@gmail.com>
2025-02-16 11:01:47 +0000

    [FIX]

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,4 +1,3 @@
 export const CONFIG = {
-  API_BASE_URL:
-    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api/v1",
+  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "/api/v1",
 };
