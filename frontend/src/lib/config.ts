671d39f67dfa3df825d8e3d62f72fcd0344a7e9f
khalidkhnz <eternalkhalidkhnz@gmail.com>
2025-02-16 14:01:59 +0000

    [TEST]

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -2,3 +2,5 @@ export const CONFIG = {
   API_BASE_URL:
     process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.khalidkhnz.in/api/v1",
 };
+
+console.log({ BASE: process.env.NEXT_PUBLIC_API_BASE_URL });
